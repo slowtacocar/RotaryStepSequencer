e5c97780f7faf62595b252a0bbbe7c2b6616a248
Bobby George <slowtacocar@gmail.com>
2021-05-20 12:29:04 -0400

    Initial Commit

---
+++ RotaryStepSequencer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RotaryStepSequencer
+//
+//  Created by Bobby George on 5/20/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
